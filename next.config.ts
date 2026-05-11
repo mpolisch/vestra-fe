dc2e0db1bbab80ff81c0a32b3b25b291ff14745f
Matthew <mpolisch@uoguelph.ca>
2026-05-11 12:30:20 -0400

    configured next.js security headers, and added robots.txt to tell web crawlers to not index pages other than home.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-    /* config options here */
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    { key: 'X-Frame-Options', value: 'DENY' },
+                    { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(), microphone=(), geolocation=()',
+                    },
+                ],
+            },
+        ];
+    },
 };
-
 export default nextConfig;
